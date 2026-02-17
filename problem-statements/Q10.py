3388ff421d8c5ca1e6ddd104029f2a622cfd2770
ANIMIREDDY YASWANTH <yaswanthanimireddy1@gmail.com>
2026-02-17 16:51:25 +0530

    Delete problem-statements/Q10.py

--- problem-statements/Q10.py
+++
@@ -1,8 +0,0 @@
-def organize_scores(scores: list[int], descending: bool) -> list[int]:
-   
-  
-    if descending:
-        return sorted(scores, reverse=True)   
-    else:
-        return sorted(scores)                 
-

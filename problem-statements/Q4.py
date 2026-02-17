78db79c224fd67dbb02d6003c7ce820524ea3ca1
ANIMIREDDY YASWANTH <yaswanthanimireddy1@gmail.com>
2026-02-17 15:31:45 +0530

    Refactor get_ticket_price function for simplicity

--- problem-statements/Q4.py
+++ problem-statements/Q4.py
@@ -1,21 +1,9 @@
-def get_ticket_price(age: int, is_student: bool) -> int:
+def get_ticket_price(age, is_student):
     if age < 12:
         return 8
-    elif age >= 65:
+    if age >= 65:
         return 10
-    else:
-        if is_student:
-            return 12
-        else:
-            return 15
+    if is_student:
+        return 12
+    return 15
 
-
-if __name__ == "__main__":
-    age_input = int(input("Enter age: "))
-    student_input = input("Is the person a student? (True/False): ")
-
-    # Convert string input to boolean
-    is_student = student_input.strip().lower() == "true"
-
-    price = get_ticket_price(age_input, is_student)
-    print("Ticket price:", price)

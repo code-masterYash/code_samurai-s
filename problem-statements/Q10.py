664f66491bebf37e74a8b72c8cf84ec73365fb17
ANIMIREDDY YASWANTH <yaswanthanimireddy1@gmail.com>
2026-02-17 16:31:03 +0530

    Create organize_scores function for sorting scores
    Add function to organize scores in ascending or descending order based on input.

---
+++ problem-statements/Q10.py
@@ -0,0 +1,8 @@
+def organize_scores(scores: list[int], descending: bool) -> list[int]:
+   
+  
+    if descending:
+        return sorted(scores, reverse=True)   
+    else:
+        return sorted(scores)                 
+
